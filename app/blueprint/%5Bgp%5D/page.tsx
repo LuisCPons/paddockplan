b074e22c02d9b0b5c74f355d005dd3c0bd41441a
LuisCPons <luis.canadilla@ie.edu>
2026-04-09 14:15:38 +0200

    Test2

--- app/blueprint/%5Bgp%5D/page.tsx
+++ app/blueprint/%5Bgp%5D/page.tsx
@@ -9,8 +9,7 @@ export async function generateStaticParams() {
 }
 
 export default async function BlueprintPage({ params }: { params: Promise<{ gp: string }> }) {
-  const resolvedParams = await params;
-  const gpKey = resolvedParams?.gp?.toLowerCase();
+  const gpKey = (await params).gp.toLowerCase();
   
   if (!gpKey) {
     notFound();
